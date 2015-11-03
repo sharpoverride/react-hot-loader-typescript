9e7547fc8fd9d697778f5076b2b71b559c16e3b7
Mihai Lazar <mihai.lazar@evozon.com>
2015-11-03 09:46:46 +0200

    Got Typescript to play nice with React and Webpack hot loader plugin.

---
+++ src/app.tsx
@@ -0,0 +1,6 @@
+console.log('how are you?');
+import * as ReactDOM from 'react-dom';
+import * as React from 'react';
+import Hello from './Hello';
+
+ReactDOM.render(<Hello />, document.querySelector('#app'));
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: 'eval',
+    entry: [
+        'webpack-dev-server/client?http://localhost:5000',
+        'webpack/hot/only-dev-server',
+        './src/app.tsx'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+    },
+    module: {
+        loaders: [{
+            test: /\.tsx?$/,
+            loaders: ['react-hot', 'ts-loader'],
+            include: path.join(__dirname, 'src')
+        }]
+    }
+};
